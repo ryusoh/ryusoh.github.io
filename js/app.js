35d1bf07b09d50f4586d5a9f140385feb6652ac1
ryusoh <zhuangliulz@gmail.com>
2025-05-19 00:41:55 -0700

    refactored js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,12 @@
 /*global $, jQuery, ga, WeixinJSBridge, _report, _, Swiper, campaignTools, FastClick*/
 
-/* 
-* @Author: Jiyun
-* @Date:   2015-05-23 15:21:59
-* @Last Modified by:   Jiyun
-* @Last Modified time: 2015-06-26 17:22:55
-*/
-
 /* jshint ignore:start */
 function canvas() {
-    function Particle( x, y, radius ) {
-        this.init( x, y, radius );
+    // ----------------------------------------
+    // Particle
+    // ----------------------------------------
+    function Particle(x, y, radius) {
+        this.init(x, y, radius);
     }
 
     Particle.prototype = {
@@ -26,12 +22,13 @@ function canvas() {
             this.vx = 0.0;
             this.vy = 0.0;
         },
+
         move: function() {
             this.x += this.vx;
             this.y += this.vy;
             this.vx *= this.drag;
             this.vy *= this.drag;
-            this.theta += random( -0.5, 0.5 ) * this.wander;
+            this.theta += random(-0.5, 0.5) * this.wander;
             this.vx += sin( this.theta ) * 0.2;
             this.vy += cos( this.theta ) * 0.1;
             this.radius *= 0.96;
@@ -39,7 +36,7 @@ function canvas() {
         },
         draw: function( ctx ) {
             ctx.beginPath();
-            ctx.arc( this.x, this.y, this.radius, 0, TWO_PI );
+            ctx.arc(this.x, this.y, this.radius, 0, TWO_PI);
             ctx.fillStyle = this.color;
             ctx.fill();
         }
@@ -48,7 +45,7 @@ function canvas() {
     // Example
     // ----------------------------------------
     var MAX_PARTICLES = 280;
-    var COLOURS = [ '#3720ca', '#41a19c', '#fff428', '#F38630', '#9600c8', '#FF4E50', '#1dce39' ];
+    var COLOURS = ['#3720ca', '#41a19c', '#fff428', '#F38630', '#9600c8', '#FF4E50', '#1dce39'];
     var particles = [];
     var pool = [];
 
@@ -56,14 +53,13 @@ function canvas() {
         container: document.getElementById( 'container' )
     });
     demo.setup = function() {
-        // Set off some initial particles.
         var i, x, y;
-        for ( i = 0; i < 20; i++ ) {
-            x = ( demo.width * 0.5 ) + random( -100, 100 );
-            y = ( demo.height * 0.5 ) + random( -100, 100 );
-            demo.spawn( x, y );
+        // Set off some initial particles.
+        for (i = 0; i < 20; i++) {
+            x = (demo.width * 0.5) + random(-100, 100);
+            y = (demo.height * 0.5) + random(-100, 100);
+            demo.spawn(x, y);
         }
-
         this.r = random(30, 80);
         this.g = random(80, 180);
         this.b = random(70, 180);
@@ -71,13 +67,13 @@ function canvas() {
     demo.spawn = function( x, y ) {
         if ( particles.length >= MAX_PARTICLES )
             pool.push( particles.shift() );
-        particle = pool.length ? pool.pop() : new Particle();
+        var particle = pool.length ? pool.pop() : new Particle();
         particle.init(x, y, random(5, 10));
         particle.wander = random(0.5, 2.0);
         particle.color = random(COLOURS);
         particle.drag = random(0.5, 0.99);
-        theta = random(TWO_PI);
-        force = random(2, 8);
+        var theta = random(TWO_PI);
+        var force = random(2, 8);
         particle.vx = sin(theta) * force;
         particle.vy = cos(theta) * force;
         particles.push(particle);
@@ -95,174 +91,26 @@ function canvas() {
     };
     demo.draw = function() {
         demo.globalCompositeOperation  = 'lighter';
-        // particles.length = 20;
         for ( var i = particles.length - 1; i >= 0; i-- ) {
             particles[i].draw( demo );
         }
 
         var grd = this.createLinearGradient(10, 0, this.width - 10, this.height);
-            // console.log(~~this.r, ~~this.g, ~~this.b);
-            // this.fillStyle = 'rgb(' + ~~this.r + ',' + ~~this.g + ',' + ~~this.b + ')';
-        
         grd.addColorStop(0, 'rgb(' + ~~this.r + ',' + ~~this.b + ',' + ~~this.g + ')');
-        // grd.addColorStop(0.4, 'rgb(' + ~~(this.r - 30) + ',' + ~~(this.g - 30) + ',' + (~~this.b - 30) + ')');
         grd.addColorStop(1, 'rgb(' + ~~(this.g) + ',' + ~~(this.r) + ',' + ~~(this.b) + ')');
         this.fillStyle = grd;
         this.fillRect(0, 0, this.width, this.height);
     };
     demo.mousemove = function() {
         var particle, theta, force, touch, max, i, j, n;
-        for ( i = 0, n = demo.touches.length; i < n; i++ ) {
+        for (i = 0, n = demo.touches.length; i < n; i++) {
             touch = demo.touches[i];
             max = random(1, 4);
-            for ( j = 0; j < max; j++ ) {
-              demo.spawn( touch.x, touch.y );
+            for (j = 0; j < max; j++) {
+                demo.spawn(touch.x, touch.y);
             }
         }
-        
-        // change background color
-        // this.r = 155 * ( this.mouse.x / this.width );
-        // this.g = 155 * ( this.mouse.y / this.height );
-        // this.b = 155 * abs( cos( PI * this.mouse.y / this.width ) );
     };
 }
 canvas();
 /* jshint ignore:end */
-
-// var mySwiper;
-
-
-// function swiperSetup() {
-//     var animated = false;
-//     mySwiper = new Swiper('#slider', {
-//         direction: 'horizontal',
-//         // direction: 'vertical',
-//         loop: false,
-//         // slidesPerView: 1,
-//         slidesPerView: 'auto',
-//         centeredSlides: true,
-//         // nextButton: '.next',
-//         keyboardControl: true,
-//         pagination: '.swiper-pagination',
-//         // mousewheelControl: true,
-//         // mousewheelForceToAxis: true,
-//         // hashnav: true,
-//         // speed: 400,
-//         // freeMode: true,
-//         spaceBetween: 20,
-
-//         onTransitionStart: function (swiper) {
-//             var index = swiper.activeIndex;
-//         },
-
-//         onTransitionEnd: function (swiper) {
-
-//         }
-//     });
-
-//     mySwiper.on('slideChangeStart', function (swiper) {
-        
-//     });
-// }
-
-
-$(function (){
-
-    // var worksArray = [
-    //     {
-    //         'title': '这是1',
-    //         'image': 'http://perber.qiniudn.com/U2NyZWVuU2hvdDIwMTUtMDQtMDZhdDEyLjQzLjA1QU0ucG5n_1431154498673',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }, {
-    //         'title': '这是2',
-    //         'image': 'http://perber.qiniudn.com/cDIyMjYwMTIyMzcuanBn_1431154515785',
-    //         'url': 'http://www.doubam.com'
-    //     }
-    // ];
-
-    // var render = {
-    //     templates: {
-    //         worksTpl: _.template($('script#worksTpl').html())
-    //     },
-
-    //     renderWorks: function () {
-
-    //         $('#works-wrap').html(render.templates.worksTpl({
-    //             works: worksArray
-    //         }));
-
-    //         // setTimeout(function () {
-    //             // 翻页插件启动
-    //             swiperSetup();
-    //         // }, 500);
-            
-
-
-    //         $('#cont').velocity({
-    //             translateY: - ($('#cont').height()) + 70
-    //         }, {
-    //             // duration: 650,
-    //             complete: function () {
-                    
-    //                 $('#works-wrap').css('z-index', 0).velocity('transition.slideUpIn', {
-    //                     complete: function () {
-                            
-    //                     }
-    //                 });
-    //             }
-    //         });
-    //     }
-    // };
-
-    // $('#nav a').click(function () {
-    //     var $t = $(this);
-    //     var target = $t.attr('data-link');
-
-    //     switch (target) {
-    //         case 'works':
-    //             render.renderWorks();
-    //             break;
-    //     }
-    // });
-
-});
--- js/template.js
+++ js/template.js
@@ -1,24 +1,17 @@
-/* 
-* @Author: Jiyun
-* @Date:   2015-05-27 23:52:45
-* @Last Modified by:   Jiyun
-* @Last Modified time: 2015-05-27 23:52:49
-*/
-
 ;(function (window) {
   var _ = _ || {};
 
   var each = _.each = _.forEach = function(obj, iterator, context) {
     if (obj == null) return obj;
     if (obj.length === +obj.length) {
-      for (var i = 0, length = obj.length; i < length; i++) {
-        if (iterator.call(context, obj[i], i, obj) === {}) return;
-      }
+        for (var i = 0, length = obj.length; i < length; i++) {
+            if (iterator.call(context, obj[i], i, obj) === {}) return;
+        }
     } else {
-      var keys = _.keys(obj);
-      for (var i = 0, length = keys.length; i < length; i++) {
-        if (iterator.call(context, obj[keys[i]], keys[i], obj) === {}) return;
-      }
+        var keys = _.keys(obj);
+        for (var i = 0, length = keys.length; i < length; i++) {
+            if (iterator.call(context, obj[keys[i]], keys[i], obj) === {}) return;
+        }
     }
     return obj;
   };
@@ -26,11 +19,11 @@
   // Fill in a given object with default properties.
   _.defaults = function(obj) {
     each(Array.prototype.slice.call(arguments, 1), function(source) {
-      if (source) {
-        for (var prop in source) {
-          if (obj[prop] === void 0) obj[prop] = source[prop];
+        if (source) {
+            for (var prop in source) {
+                if (obj[prop] === void 0) obj[prop] = source[prop];
+            }
         }
-      }
     });
     return obj;
   };
@@ -58,8 +51,8 @@
     escape: {
       '&': '&amp;',
       '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
+      '>': '&gt;', // IE < 9 does not support &apos;
+      '"': '&quot;', // IE < 9 does not support &apos;
       "'": '&#x27;'
     }
   };
@@ -71,9 +64,9 @@
 
   _.escape = function(string) {
     if (string == null) return '';
-    return ('' + string).replace(entityRegexes.escape, function(match) {
-      return entityMap.escape[match];
-    });
+      return ('' + string).replace(entityRegexes.escape, function(match) {
+          return entityMap.escape[match];
+      });
   };
 
   _.templateSettings = {
@@ -118,41 +111,41 @@
     // Compile the template source, escaping string literals appropriately.
     var index = 0;
     var source = "__p+='";
-    text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
-      source += text.slice(index, offset)
-        .replace(escaper, function(match) { return '\\' + escapes[match]; });
+      text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
+          source += text.slice(index, offset)
+              .replace(escaper, function(match) { return '\\' + escapes[match]; });
 
-      if (escape) {
-        source += "'+\n((__t=(" + escape + "))==null?'':_.escape(__t))+\n'";
-      }
-      if (interpolate) {
-        source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
-      }
-      if (evaluate) {
-        source += "';\n" + evaluate + "\n__p+='";
-      }
-      index = offset + match.length;
-      return match;
-    });
+          if (escape) {
+              source += "'+\n((__t=(" + escape + "))==null?'':_.escape(__t))+\n'";
+          }
+          if (interpolate) {
+              source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+          }
+          if (evaluate) {
+              source += "';\n" + evaluate + "\n__p+='";
+          }
+          index = offset + match.length;
+          return match;
+      });
     source += "';\n";
 
     // If a variable is not specified, place data values in local scope.
     if (!settings.variable) source = 'with(obj||{}){\n' + source + '}\n';
 
     source = "var __t,__p='',__j=Array.prototype.join," +
-      "print=function(){__p+=__j.call(arguments,'');};\n" +
-      source + "return __p;\n";
+        "print=function(){__p+=__j.call(arguments,'');};\n" +
+        source + "return __p;\n";
 
     try {
-      render = new Function(settings.variable || 'obj', '_', source);
+        render = new Function(settings.variable || 'obj', '_', source);
     } catch (e) {
-      e.source = source;
-      throw e;
+        e.source = source;
+        throw e;
     }
 
     if (data) return render(data, _);
     var template = function(data) {
-      return render.call(this, data, _);
+        return render.call(this, data, _);
     };
 
     // Provide the compiled function source as a convenience for precompilation.
