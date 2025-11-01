4bb31a7fef9f46d4245567efaa3de5af66c5d5be
ryusoh <zhuangliulz@gmail.com>
2025-11-01 00:36:30 -0700

    Bundle Font Awesome locally

--- js/loader/vendorLoader.js
+++ js/loader/vendorLoader.js
@@ -4,21 +4,12 @@
         if (!window.CDNLoader) {
             return;
         }
-        const fontAwesome = [
-            'https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css',
-            'https://unpkg.com/font-awesome@4.7.0/css/font-awesome.min.css',
-            'https://cdn.bootcdn.net/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-            'https://cdn.baomitu.com/font-awesome/4.7.0/css/font-awesome.min.css',
-        ];
+        const fontAwesome = ['/assets/vendor/font-awesome/css/font-awesome.min.css'];
         const googleFonts = [
             'https://fonts.googleapis.com/css2?family=Lobster&display=swap',
             'https://fonts.bunny.net/css?family=Lobster',
         ];
         window.CDNLoader.preconnect([
-            'https://cdn.jsdelivr.net',
-            'https://unpkg.com',
-            'https://cdn.bootcdn.net',
-            'https://cdn.baomitu.com',
             'https://fonts.googleapis.com',
             'https://fonts.gstatic.com',
             'https://fonts.bunny.net',
